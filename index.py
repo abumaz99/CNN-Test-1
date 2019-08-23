0639c6e143d05c2669f04778f25c0e08dd02f2dc
abumaz99 <46001820+abumaz99@users.noreply.github.com>
2019-08-23 22:17:57 +0100

    Added main file.

---
+++ index.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import tensorflow as tf 
+from tensorflow.python.framework import ops
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from tensorflow.python.keras.utils.np_utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+np.random.seed(2)
+
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+
+x_train = train.drop(['label'], axis=1).values.astype('float32') 
+y_train = train['label'].values.astype('int32') 
+x_test = test.values.astype('float32')
+x_train = x_train.reshape(x_train.shape[0], 28, 28) / 255.0
+x_test = x_test.reshape(x_test.shape[0], 28, 28) / 255.0
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.10, random_state=2)
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28,1)  
+x_val = x_val.reshape(x_val.shape[0], 28, 28,1)  
+x_test = x_test.reshape(x_test.shape[0], 28, 28,1) 
+
+def greatModel(input_shape):
+
+    X_input = Input(input_shape)
+
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
+    X = BatchNormalization(axis=3, name='bn0')(X)
+    X = Activation('relu')(X)
+
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+
+    X = Flatten()(X)
+    X = Dense(10, activation='softmax', name='fc')(X)
+
+    model = Model(inputs=X_input, outputs=X, name='greatModel')
+
+    return model
+    
+
+model_run = greatModel(x_train.shape[1:])
+
+model_run.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model_run.fit(x_train, y_train, epochs=3, batch_size=64)
+
+preds = model_run.predict(x_test, batch_size=64)
+print()
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+
+
